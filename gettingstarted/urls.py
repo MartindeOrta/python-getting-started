bcc143b4d0b93d70e002a3ef77607a0be87f3e46
MartindeOrta <82926690+MartindeOrta@users.noreply.github.com>
2023-10-19 16:38:50 -0300

    Update urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -15,7 +15,7 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.actualizador, name="index"),
+    path("", hello.views.monitor_futuros, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
        path('monitor/', hello.views.monitor_futuros),
